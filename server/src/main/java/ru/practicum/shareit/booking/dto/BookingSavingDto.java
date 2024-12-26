d16bc288dab6b6ab9bfdedd092a9e3bdf6db7bff
3agent999 <agentsmith999@yandex.ru>
2024-12-27 00:58:12 +0300

    правки

--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingSavingDto.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingSavingDto.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.booking.dto;
 
-import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.PastOrPresent;
